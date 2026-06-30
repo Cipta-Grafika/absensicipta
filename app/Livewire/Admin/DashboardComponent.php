ce620563150aecf93b1b79775b8729f4310c9f60
astrocoding <zaenalalfian20@gmail.com>
2026-06-30 22:58:58 +0700

    fix(dashboard): fixed pagination behavior, UI, layout

--- app/Livewire/Admin/DashboardComponent.php
+++ app/Livewire/Admin/DashboardComponent.php
@@ -9,10 +9,13 @@ use App\Models\User;
 use Illuminate\Database\Eloquent\Collection;
 use Livewire\Attributes\Url;
 use Livewire\Component;
+use Livewire\WithPagination;
 
 class DashboardComponent extends Component
 {
     use AttendanceDetailTrait, HasAttendanceSummary;
+    use WithPagination;
+
 
     #[Url]
     public $date = '';
@@ -22,6 +25,25 @@ class DashboardComponent extends Component
 
     #[Url]
     public $month = '';
+
+    public function updating($key): void
+    {
+        if ($key === 'month') {
+            $this->resetPage();
+            $this->week = '';
+            $this->date = '';
+        }
+        if ($key === 'week') {
+            $this->resetPage();
+            $this->month = '';
+            $this->date = '';
+        }
+        if ($key === 'date') {
+            $this->resetPage();
+            $this->month = '';
+            $this->week = '';
+        }
+    }
 
     public function render()
     {
