b518783680b7f3118a85eca59b7116decaaf0ee3
astrocoding <zaenalalfian20@gmail.com>
2026-07-21 08:47:18 +0700

    feats(error); added client error and server error page custom

---
+++ resources/views/errors/401.blade.php
@@ -0,0 +1,5 @@
+<x-error-page 
+  code="401" 
+  title="Tidak Terautentikasi" 
+  message="Sesi Anda mungkin telah kedaluwarsa atau Anda belum login. Silakan login kembali untuk melanjutkan." 
+/>
---
+++ resources/views/errors/429.blade.php
@@ -0,0 +1,5 @@
+<x-error-page 
+  code="429" 
+  title="Terlalu Banyak Permintaan" 
+  message="Anda telah mengirimkan terlalu banyak permintaan ke server dalam waktu singkat. Harap tunggu beberapa saat sebelum mencoba lagi." 
+/>
